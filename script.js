18bf18090a2cc7eced1db76669086c3c1370260b
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-21 11:13:08 +0100

    made basic logic, adding books manually rn and displaying them on a page

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+//add book to an array
+//display that book on the page
+// make sure this now works for multiple books
+
+
+const myLibrary = [];
+
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+Book.prototype.addBookToLibrary = function(){
+    myLibrary.push(`${this.title} written by ${this.author} has ${this.pages} pages, ${this.read}`);
+}
+
+
+const book1 = new Book("Hobbit", "J.R.R Tolkien", 467, "read");
+const book2 = new Book("LoTR", "J.R.R Tolkien", 398, "not read");
+
+
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+
+
+const content = document.querySelector("#content");
+const para = document.createElement("p");
+para.textContent = myLibrary;
+content.appendChild(para);
