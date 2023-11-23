eedbf528b50d42c5f11208719c69bf17a72ff7a4
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-11-23 13:40:53 +0100

    logic now works except for the read/unread button

--- script.js
+++ script.js
@@ -1,42 +1,78 @@
 const myLibrary = [];
 let counter = 0;
+let paraCounter = 0;
+let dataCounter = 0;
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, data){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.data = data;
 };
 
 Book.prototype.addBookToLibrary = function(){
-    myLibrary.push(`${this.title}, ${this.author}, ${this.pages},${this.read}`);
-}
+    myLibrary.push({
+        title: this.title,
+        author: this.author,
+        pages: this.pages,
+        read: this.read,
+        data: dataCounter++
+    });
+    paraCounter++;
+};
 
-// const testBook = new Book("Hobbit", "J.R.R Tolkien", 467, "read");
-// testBook.addBookToLibrary();
+//  Book.prototype.changeRead = function(){
+//      //find para with correct id and change html
+    
 
-function updateDisplay(){
+// };
+
+
+Book.prototype.updateDisplay = function(){
     const content = document.querySelector("#content");
     const para = document.createElement("p");
+    para.setAttribute("data-id", paraCounter -1);
     const delBtn = document.createElement("button");
     const readBtn = document.createElement("button");
-    para.textContent = myLibrary[counter];
+    readBtn.setAttribute("data-id",paraCounter -1);
+    para.textContent = `Book title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Read: ${this.read}`;
     content.appendChild(para);
     para.appendChild(readBtn);
     para.appendChild(delBtn);
+    let thisButtonId = readBtn.getAttribute("data-id");
+    let thisParaId = para.getAttribute("data-id");
 
+    const getParaID = delBtn.parentElement.dataset.id;
+    
     readBtn.innerHTML = "Read/Unread";
     readBtn.addEventListener("click", ()=>{
-        console.log(myLibrary[counter]);
-    });
+
+        // for (let i =0; i < myLibrary.length; i++){
+        //     if(myLibrary[i].data == getParaID){
+        //         if(this.read == false){
+        //             read.value = true;
+        //         }else if(this.read == true){
+        //             read.value = false;
+        //         }
+
+        //     }
+        //     console.log(myLibrary);
+            
+        };
+      
+});
 
     delBtn.innerHTML = "Delete";
     delBtn.addEventListener("click", ()=>{
-        myLibrary.splice(counter, 1);
-        para.remove(myLibrary);
-         if(counter > 0){
-             counter--;
-         }
+
+        for (let i =0; i < myLibrary.length; i++){
+            if(myLibrary[i].data == getParaID){
+                myLibrary.splice(i,1)
+                para.remove();
+            }         
+        };
+    console.log(myLibrary)
     });
 };
 
@@ -59,12 +95,14 @@ btn.addEventListener("click", ()=>{
     const pages = pagesNumForm.value;
     const read = readForm.checked;
 
+    
+
 
     let book = new Book(title, author, pages, read);
     book.addBookToLibrary();
-    updateDisplay();
-    console.log(myLibrary);
-    console.log(counter);
+    book.updateDisplay();
+    // console.log(myLibrary);
+    console.log("this is basic counter: " + counter);
 
     bookNameForm.value = "";
     authorForm.value = "";
